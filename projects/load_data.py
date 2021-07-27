3eda2b0ae993a92e90f517a3758eec38fcaa689c
and88x <fernando.garcia88x@gmail.com>
2021-07-27 17:47:45 -0500

    afp001 added

--- projects/load_data.py
+++ projects/load_data.py
@@ -1,6 +1,11 @@
 import json
 from projects.models import My_Portfolio
 
+# python manage.py flush
+# python manage.py shell
+# import projects.load_data
+
+GITHUB_URL = "https://raw.githubusercontent.com/and88x/af_portfolio/50c7f7fecc3c60d5f70247f5f5dbe88238c65f49/projects/static/"
 # Opening JSON file
 with open('./projects/static/data.json') as json_file:
     data = json.load(json_file)
@@ -14,8 +19,8 @@ with open('./projects/static/data.json') as json_file:
             sdescription = i["sdescription"],
             ldescription = i["ldescription"],
             technology   = i["technology"],
-            image        = i["image"],
-            second_img   = i["second_img"],
+            image        = GITHUB_URL+i["image"],
+            second_img   = GITHUB_URL+i["second_img"],
             link         = i["link"]
         )
         p[j].save()     
